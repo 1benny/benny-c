bc3afa7bf1c15d7cb48f32aff415aa8d63752147
bdxves <bdives@icloud.com>
2024-07-13 03:50:13 +1000

    reduced array size deleted shit

--- Programs/pubs.c
+++ Programs/pubs.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <limits.h>
 
 int main(int argc, char* argv[]) {
     FILE* process;
-    char cmd[4096] = "curl -s https://pubs.opengroup.org/onlinepubs/009696799/functions/";
-    char pipe[200] = " | pandoc -f html -t plain";
-    // Uses the program "pandoc" to convert html to plain text after curl downloads the web page.
+    char cmd[128] = "curl -s https://pubs.opengroup.org/onlinepubs/009696799/functions/";
+    char pipe[32] = " | pandoc -f html -t plain";
+    // Uses pandoc to convert html to plain text after curl transfers the web page.
 
     // If the program receives any argument it attempts to run the search and convert
     if (argc > 1) {
